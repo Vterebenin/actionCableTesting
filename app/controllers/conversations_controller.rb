d3bd03abc5138a2ece0173fc00b29c49a7075d75
Vterebenin <niceserver11@mail.ru>
2019-05-08 02:11:43 +0300

    add find battle function

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,6 +9,37 @@ class ConversationsController < ApplicationController
 		respond_to do |format|
 			format.js
 		end
+		respond_to do |format|
+			format.js {render inline: "location.reload();" }
+		end
+	end
+
+	$users = []
+	# POST /ajax/sum
+	def ajax_sum
+		@user = current_user
+		puts @user.id
+		if !$users.include? @user.id
+			$users.push(@user.id)
+		end
+		puts "USERS:" + $users.to_s
+		# Do something with input parameter and respond as JSON with the output
+		if $users.length > 1 
+			puts "match found"
+			# here is the connection
+			# $users.pop(current_user.id)
+			puts "USERS AFTER POP" + $users.to_s
+			@conversation = Conversation.get(current_user.id, $users.first)
+			add_to_conversations unless conversated?
+			respond_to do |format|
+				format.js
+			end
+			respond_to do |format|
+				format.js {render inline: "location.reload();" }
+			end
+			$users.clear
+			puts "CLEARED"
+		end
 	end
 
 	def close
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users
+  post '/' => 'conversations#ajax_sum'
 
   resources :conversations, only: [:create] do
     member do
